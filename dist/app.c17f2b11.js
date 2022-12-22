de1dded97eeeec060ce92d92599b1583b5172e18
Michael Kenyi <michaelkenyi13@gmail.com>
2022-12-22 23:44:25 +0600

    footer

--- dist/app.c17f2b11.js
+++ dist/app.c17f2b11.js
@@ -17759,7 +17759,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50488" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56706" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
